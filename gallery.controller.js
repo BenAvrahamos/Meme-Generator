9c3bcd6ddd9f6a3552b6bb964710fea665845f92
BenAvrahamos <benavraham1998@gmail.com>
2024-02-29 09:09:09 +0200

    Add grid layout and a searchbar to gallery

--- gallery.controller.js
+++ gallery.controller.js
@@ -2,16 +2,16 @@
 
 renderGallery()
 function renderGallery() {
-    const elGallery = document.querySelector('.gallery')
+    const elGallery = document.querySelector('.gallery-img-container')
 
     const imgs = getImg()
     console.log(imgs.id);
-    const strHtml = imgs.map(img => `<img src="img/${img.id}.jpg" id="${img.id}" onclick="onSelectImg(id)" />`
+    const strHtml = imgs.map(img => `<img class= "gallery-img"src="img/${img.id}.jpg" id="${img.id}" onclick="onSelectImg(id)" />`
 
     )
 
 
-    elGallery.innerHTML = strHtml
+    elGallery.innerHTML = strHtml.join('')
 }
 
 
