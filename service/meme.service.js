071abaebd924de7701ec0cff47d1b632aebe2fbf
BenAvrahamos <benavraham1998@gmail.com>
2024-03-01 16:19:37 +0200

    Add datalist filter and btn to clear it to gallery

--- service/meme.service.js
+++ service/meme.service.js
@@ -11,10 +11,21 @@ var gImgs = [
     { id: 8, url: 'img/8.jpg', keywords: ['awkward', 'funny'] },
     { id: 9, url: 'img/9.jpg', keywords: ['cute', 'funny', 'kids'] },
     { id: 10, url: 'img/10.jpg', keywords: ['presidents', 'funny'] },
-    { id: 11, url: 'img/11.jpg', keywords: ['dogs', 'funny'] },
-    { id: 12, url: 'img/12.jpg', keywords: ['dogs', 'funny'] },
-    { id: 13, url: 'img/13.jpg', keywords: ['dogs', 'funny'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['funny', 'cute', 'awkward'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['smart'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['funny', 'smart'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['scary', 'awkward'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['funny', 'smart'] },
+    { id: 16, url: 'img/16.jpg', keywords: ['funny', 'cute'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['sad', 'awkward'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['kids', 'funny'] },
 ]
+
+const gImgFilter = {
+    filterBy: {
+        category: '',
+    },
+}
 
 var gMeme = {
     selectedImgId: null,
@@ -39,7 +50,9 @@ var gMeme = {
 }
 
 function getImg() {
-    return gImgs
+    if (!gImgFilter.filterBy.category) return gImgs
+    const filteredImgs = gImgs.filter(img => img.keywords.includes(gImgFilter.filterBy.category))
+    return filteredImgs
 }
 
 function getGMeme() {
@@ -106,7 +119,7 @@ function alignText(dir) {
 
 
 function addLine(gElCanvas) {
-    
+
     const margin = (gMeme.lines.length - 1) * gElCanvas.height / 8
 
     gMeme.lines.push(
@@ -137,23 +150,35 @@ function setLineCoords(indx, x, y, textWidth, textHight) {
 
     gMeme.lines[indx].borderEndX = x + textWidth
     gMeme.lines[indx].borderEndY = y + textHight
-    
+
 
 
 }
 
 
-function moveKeyUp(value){
+function moveKeyUp(value) {
     if (!gMeme.lines.length) return
     gMeme.lines[gMeme.selectedLineIdx].posY += value
 }
 
-function deleteLine(){
+function deleteLine() {
 
-const indx = gMeme.selectedLineIdx
-gMeme.lines.splice(indx, 1)
+    const indx = gMeme.selectedLineIdx
+    gMeme.lines.splice(indx, 1)
 
-gMeme.selectedLineIdx = indx -1
+    gMeme.selectedLineIdx = indx - 1
 
+}
+
+
+function filterImgs(category) {
+
+    gImgFilter.filterBy.category = category
+
+}
+
+
+function clearFilter(){
+    gImgFilter.filterBy.category = ''
 }
 
